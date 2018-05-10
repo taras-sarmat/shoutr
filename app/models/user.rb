143d402a90b1e13e859d046a41349adb051c955c
taras_sarmat <taras_sarmat@ukr.net>
2018-05-10 16:34:38 +0300

    Add ability to follow and unfollow

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
   has_many :likes
   has_many :liked_shouts, through: :likes, source: :shout
+  
+  has_many :followed_user_relationships,
+           foreign_key: 'follower_id',
+           class_name: 'FollowingRelationship',
+           dependent: :destroy  
+  has_many :followed_users, through: :followed_user_relationships
 
   def like(shout)
   	liked_shouts << shout
@@ -14,11 +20,23 @@ class User < ApplicationRecord
     liked_shouts.destroy(shout)
   end
   
+  def following?(user)
+    followed_user_ids.include?(user.id)
+  end
+
   def liked?(shout)
   	liked_shout_ids.include?(shout.id)
   end
 
   def to_param
     username
+  end
+
+  def follow(user)
+    followed_users << user
+  end
+
+  def unfollow(user)
+    followed_users.delete(user)
   end
 end
---
+++ config/followed_users_controller.rb
@@ -0,0 +1,3 @@
+Searching 8 files for "followed_users_controller.rb"
+
+0 matches
