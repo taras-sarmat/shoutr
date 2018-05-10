5e53c8809729fefb85d56063e6cbbd9572034608
taras_sarmat <taras_sarmat@ukr.net>
2018-05-10 12:14:48 +0300

    Added PhotoShout

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180510082922) do
+ActiveRecord::Schema.define(version: 20180510085601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "guests", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photo_shouts", force: :cascade do |t|
+    t.string "image_file_name", null: false
+    t.string "image_content_type", null: false
+    t.integer "image_file_size", null: false
+    t.datetime "image_updated_at", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
