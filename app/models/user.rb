6e2540cef26e63a38f4f2dc1793f0b8288674455
taras_sarmat <taras_sarmat@ukr.net>
2018-05-10 17:24:29 +0300

    Fixed relations

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,19 +9,23 @@ class User < ApplicationRecord
   has_many :followed_user_relationships,
            foreign_key: 'follower_id',
            class_name: 'FollowingRelationship',
-           dependent: :destroy  
+           dependent: :destroy
   has_many :followed_users, through: :followed_user_relationships
 
-  has_many :followers, 
-            through: :followed_user_relationships, 
-            class_name: 'FollowingRelationship',
-            dependent: :destroy
-  has_many :follower_relations, foreign_key: :followed_user_id
+  has_many :follower_relationships,
+           foreign_key: 'followed_user_id',
+           class_name: 'FollowingRelationship',
+           dependent: :destroy
+  has_many :followers, through: :follower_relationships
 
   def like(shout)
   	liked_shouts << shout
   end
 
+  def timeline_shouts
+    Shout.where(user_id: followed_user_ids + [id])  
+  end
+  
   def unlike(shout)
     liked_shouts.destroy(shout)
   end
